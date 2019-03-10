d3179ce49ed0ac5c44ddec4cee4d1dcc419b7656
rustgainutdinov <46785191+rustgainutdinov@users.noreply.github.com>
2019-03-10 21:28:04 +0300

    change logs load method

--- database/queries/index.js
+++ database/queries/index.js
@@ -1,110 +1,133 @@
 const queries = {
-  'test-select': {
-    sql: `SELECT cdn from urls WHERE wpt_id = '190202_00_67083ef37280f0cb02f1727883c71d70';`,
-    accessRight: 900
-  },
-  'test-insert': {
-    sql: `INSERT INTO users(users_id, name, last_name, email, pass) VALUES('382f6fdf-33a6-467b-89aa-62320d563be7', 'name', 'last_name', 'email', 'pass');`,
-    accessRight: 900
-  },
-  'create-user': {
-    sql:
-      `INSERT INTO 
+	'test-select': {
+		sql: `SELECT cdn from urls WHERE wpt_id = '190202_00_67083ef37280f0cb02f1727883c71d70';`,
+		accessRight: 900
+	},
+	'test-insert': {
+		sql: `INSERT INTO users(users_id, name, last_name, email, pass) VALUES('382f6fdf-33a6-467b-89aa-62320d563be7', 'name', 'last_name', 'email', 'pass');`,
+		accessRight: 900
+	},
+	'create-user': {
+		sql:
+			`INSERT INTO 
       users(name, last_name, email, pass, access_rights_id, auth_token, access_rights_id) 
       VALUES 
       (?(name), ?(lastName), ?(email), ?(pass), ?(accessRightsId), ?(authToken), '0387c26f-ec0d-40a7-a901-a0b0b8162871');`,
-    accessRight: 500
-  },
-  'get-users-data-by-token': {
-    sql:
-    `SELECT 
+		accessRight: 500
+	},
+	'get-users-data-by-token': {
+		sql:
+			`SELECT 
     access_right
     FROM users
     LEFT JOIN access_rights ON users.access_rights_id = access_rights.access_rights_id
     WHERE auth_token = ?(token);`
-  },
-  'auth-user': {
-    sql:
-      `SELECT 
+	},
+	'auth-user': {
+		sql:
+			`SELECT 
       auth_token, pass, access_right
       FROM users
       LEFT JOIN access_rights ON users.access_rights_id = access_rights.access_rights_id
       WHERE email = ?(email);`
-  },
-  'update-user-token': {
-    sql:
-      `UPDATE users
+	},
+	'update-user-token': {
+		sql:
+			`UPDATE users
       SET auth_token = ?(newToken)
       WHERE auth_token = ?(token);`
-  },
-  'get-domain-count': {
-    sql:
-      `SELECT 
+	},
+	'get-domain-count': {
+		sql:
+			`SELECT 
       domains_id
       FROM domains
       WHERE domain = ?(domain)`,
-    accessRight: 500
-  },
-  'insert-domain': {
-    sql:
-      `INSERT 
+		accessRight: 500
+	},
+	'insert-domain': {
+		sql:
+			`INSERT 
       INTO domains
       (domains_id, domain) 
       VALUES
       (?(domainsId), ?(domain))`,
-    accessRight: 500
-  },
-  'update-url-log': {
+		accessRight: 500
+	},
+	'update-url-log': {
 		sql:
 			`UPDATE urls
 			SET ip = ?(ip), location = ?(location), browser = ?(browser), wpt_id = ?(wptId), date_time = ?(dateTime), counter = ?(counter)
 			WHERE urls_id = ?(urlsId);`,
-  },
-  'insert-url': {
-    sql:
-      `INSERT 
+	},
+	'insert-url': {
+		sql:
+			`INSERT 
       INTO urls
-      (domains_id, url, ip, location, browser, wpt_id, date_time) 
+      (domains_id, url, ip, location, browser, wpt_id, date_time, api, counter)
       VALUES
-      (?(domainsId), ?(url), ?(ip), ?(location), ?(browser), ?(wptId), ?(dateTime))`,
-    accessRight: 500
-  },
-  'update-url': {
-    sql: `UPDATE urls
+      (?(domainsId), ?(url), ?(ip), ?(location), ?(browser), ?(wptId), ?(dateTime), ?(api), ?(counter))`,
+		accessRight: 500
+	},
+	'update-url': {
+		sql: `UPDATE urls
       SET load_time = ?(loadTime), ttfb = ?(ttfb), bytes = ?(bytes), cdn = ?(cdn) WHERE wpt_id = ?(wptId)`,
-    accessRight: 500
-  },
-  'count-logs': {
-    sql:
-      `SELECT 
+		accessRight: 500
+	},
+	'count-logs': {
+		sql:
+			`SELECT 
       COUNT(urls_id)
       FROM urls
       WHERE date_time >= ?(startDate) AND date_time < ?(endDate)`,
-    accessRight: 500
-  },
-  'get-urls-by-date': {
-    sql:
-      `SELECT
-      urls_id AS id, domain, url, ip, robot, cms, wpt_id as wptId, load_time AS loadTime, ttfb, bytes, http_2 AS http2, location, browser, cdn, date_time, counter
+		accessRight: 500
+	},
+	'get-urls-by-date': {
+		sql:
+			`SELECT
+      urls_id AS id, domain, url, ip, robot, api, cms, wpt_id as wptId, load_time AS loadTime, ttfb, bytes, http_2 AS http2, location, browser, cdn, date_time, counter
       FROM urls
       INNER JOIN domains ON domains.domains_id = urls.domains_id
       WHERE date_time >= ?(startDate) AND date_time < ?(endDate)`,
-    accessRight: 500
-  },
-  'find-url': {
-    sql:
-      `SELECT urls_id, counter
+		accessRight: 500
+	},
+	'find-url': {
+		sql:
+			`SELECT urls_id, counter
       FROM urls 
       WHERE url = ?(url) AND date_time >= ?(startDate) AND date_time < ?(endDate)`
-  },
-  'load-all-logs-count': {
-    sql:
-      `SELECT 
+	},
+	'load-all-logs-count': {
+		sql:
+			`SELECT 
       DATE(date_time), COUNT(urls_id) 
       FROM urls 
       GROUP BY DATE(date_time);`,
 		accessRight: 500
-  }
+	},
+	'insert-log': {
+		sql:
+			`INSERT
+			INTO load_log
+			(date, api_tests, uniq_tests, total_tests)
+			VALUES
+			(?(date), ?(api), ?(uniq), ?(total))`
+	},
+	'get-log-load-data': {
+		sql:
+			`SELECT 
+			load_log_id 
+			FROM load_log
+			WHERE date = ?(date)`
+	},
+	'load-log-count-data': {
+		sql:
+			`SELECT 
+			api_tests, uniq_tests, total_tests
+			FROM 
+			load_log
+			WHERE date =?(date)`
+	}
 };
 
 module.exports = queries;
--- routes/load-logs/index.js
+++ routes/load-logs/index.js
@@ -1,52 +1,52 @@
 const database = require(`${__dirname}/../../database`);
 const User = require(`${__dirname}/../../models/user`);
-const parse = require(`${__dirname}/../../parse/log`);
+const parse = require(`${__dirname}/../../parse/logs`);
 
 function getDateAgo(date, days) {
-  date.setDate(date.getDate() + days);
-  var dd = date.getDate();
-  var mm = date.getMonth() + 1; //January is 0!
+	date.setDate(date.getDate() + days);
+	var dd = date.getDate();
+	var mm = date.getMonth() + 1; //January is 0!
 
-  var yyyy = date.getFullYear();
-  if (dd < 10) {
-    dd = '0' + dd;
-  }
-  if (mm < 10) {
-    mm = '0' + mm;
-  }
-  return yyyy + '-' + mm + '-' + dd;
+	var yyyy = date.getFullYear();
+	if (dd < 10) {
+		dd = '0' + dd;
+	}
+	if (mm < 10) {
+		mm = '0' + mm;
+	}
+	return yyyy + '-' + mm + '-' + dd;
 }
 
 function execute(req, res, next) {
-  const token = req.query.token;
-  const data = JSON.parse(req.query.data);
-  const date = new Date(data.date);
+	const token = req.query.token;
+	const data = JSON.parse(req.query.data);
+	const date = new Date(data.date);
 
-  User.getUsersData(token, (err, usersData) => {
-    if (err) return next(err);
-    const accessRight = usersData ? usersData['access_right'] : null;
+	User.getUsersData(token, (err, usersData) => {
+		if (err) return next(err);
+		const accessRight = usersData ? usersData['access_right'] : null;
 
-    const startEndDate = {
-      startDate: getDateAgo(date, 0),
-      endDate: getDateAgo(date, 1)
-    };
+		const startEndDate = {
+			startDate: getDateAgo(date, 0),
+			endDate: getDateAgo(date, 1)
+		};
 
-    database.execute('count-logs', startEndDate, accessRight, (err, result) => {
-      if (err) return next(err);
+		database.execute('get-log-load-data', {date: data.date}, accessRight, (err, result) => {
+			if (err) return next(err);
+			if (result.length === 0) {
+				const wptPath = data.date.replace(/-/ig, '');
+				parse(wptPath, data.date, (err) => {
+					if (err) return next(err);
+				});
+				return res.status(200).json({message: 'wait'});
+			}
 
-      if (Number(result[0].count) === 0) {
-        parse(data.date.replace(/-/ig, ''), startEndDate, (err) => {
-          if (err) return next(err);
-        });
-        return res.status(200).json({message: 'wait'});
-      }
-
-      database.execute('get-urls-by-date', startEndDate, accessRight, (err, result) => {
-        if (err) return next(err);
-        res.status(200).json(result);
-      });
-    });
-  });
+			database.execute('get-urls-by-date', startEndDate, accessRight, (err, result) => {
+				if (err) return next(err);
+				res.status(200).json(result);
+			});
+		});
+	});
 }
 
 module.exports = execute;
